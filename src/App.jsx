92d802f817f24707f700b1210aeab624e219b59f
Ed Westfield Jr. 西田爱德 <30934590+edwestfieldjr@users.noreply.github.com>
2022-12-06 08:44:20 -0500

    Add darktheme styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
-import React, {Fragment} from "react";
+import React, { Fragment } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 
 export default () => (
-  <Fragment>
-    <h1>Ed Westfield Jr. 西田爱德</h1>
-    <h3>Software Engineer/Licensed Attorney</h3>
-    <p>More to come...</p>
-  </Fragment>
+  <div className="container text-center p-5 mt-5 text-muted " >
+    <h1 className="display-3">Ed Westfield Jr.</h1>
+    <h3 className="display-6">西田爱德</h3>
+    <ul className="list-unstyled">
+      <li>Attorney</li>
+      <li>Graphic Designer</li>
+      <li>Web Developer</li>
+      <li>Software Engineer</li>
+    </ul>
+    <div>
+    <a className="link-secondary" href="https://github.com/edwestfieldjr">github.com/edwestfieldjr</a>
+    </div>
+  </div>
 );
