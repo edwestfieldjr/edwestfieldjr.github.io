cce97b3a6e79be6661d6031a320468ddaba86ac9
Ed Westfield Jr. 西田爱德 <30934590+edwestfieldjr@users.noreply.github.com>
2022-12-05 23:30:17 -0500

    Add nano react

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React, {Fragment} from "react";
+
+export default () => (
+  <Fragment>
+    <h1>Ed Westfield Jr. 西田爱德</h1>
+    <h3>Software Engineer/Licensed Attorney</h3>
+    <p>More to come...</p>
+  </Fragment>
+);
