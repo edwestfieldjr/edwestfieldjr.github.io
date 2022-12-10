8dd4d04cd5834be6ff2a4c7a38b0d8df7c5d7cf5
Ed Westfield Jr. 西田爱德 <30934590+edwestfieldjr@users.noreply.github.com>
2022-12-10 14:35:45 -0500

    add linkedin and main site

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 
 export default () => (
   <div className="container text-center p-1 mt-1 mt-xs-2 p-xs-2 mt-sm-3 p-sm-3 mt-md-4 p-md-4 mt-lg-5 p-lg-5 text-muted " >
-    <h1 className="display-3">Ed Westfield Jr.</h1>
+    <h1 className="display-4">Ed Westfield Jr.</h1>
     <h3 className="display-6">西田爱德</h3>
     <ul className="list-unstyled">
       <li>Attorney</li>
@@ -15,6 +15,12 @@ export default () => (
     </ul>
     <div>
     <a className="link-secondary" href="https://github.com/edwestfieldjr">github.com/edwestfieldjr</a>
+    </div>
+    <div>
+    <a className="link-secondary" href="https://linkedin.com/in/edwestfieldjr/">linkedin.com/in/edwestfieldjr/</a>
+    </div>
+    <div>
+    <a className="link-secondary" href="https://edwestfieldjr.com/">edwestfieldjr.com</a>
     </div>
   </div>
 );
